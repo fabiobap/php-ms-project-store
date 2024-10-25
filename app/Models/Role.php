6940cf1e3d6ca37a4da20bd56c434564c529b22f
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-24 22:57:33 -0300

    scope for admin/customer

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Enums\RoleNames;
 use Database\Factories\RoleFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -25,5 +27,15 @@ class Role extends Model
     public function permissions(): BelongsToMany
     {
         return $this->belongsToMany(Permission::class)->using(PermissionRole::class)->withTimestamps();
+    }
+
+    public function scopeAdmin(Builder $query)
+    {
+        return $query->where('name', RoleNames::ADMIN->value);
+    }
+
+    public function scopeCustomer(Builder $query)
+    {
+        return $query->where('name', RoleNames::CUSTOMER->value);
     }
 }
