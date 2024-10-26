45938389556855bc87230d19dc15934b38235ffc
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-25 23:29:46 -0300

    wip seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Enums\RoleNames;
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+       $rAdmin= Role::factory()->create([
+            'name' => RoleNames::ADMIN->value,
         ]);
+
+        $rCustomer = Role::factory()->create([
+            'name' => RoleNames::CUSTOMER->value,
+        ]);
+
+        User::factory()->admin()->create(
+            ['email' => 'admin@admin.org']
+        );
+
+        User::factory()->customer()->create(
+            ['email' => 'customer@customer.org']
+        );
+
+        $users = User::factory()
+            ->count(120)
+            ->state(new Sequence(
+                ['role_id' => $rAdmin->id],
+                ['role_id' => $rCustomer->id],
+            ))
+            ->create();
     }
 }
