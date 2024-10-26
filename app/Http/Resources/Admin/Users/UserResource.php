0d12beee508a8348d35864f76f69bde478a6c7d7
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-25 23:35:59 -0300

    resources for permissions/role

--- app/Http/Resources/Admin/Users/UserResource.php
+++ app/Http/Resources/Admin/Users/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Admin\Users;
 
+use App\Http\Resources\Admin\RolesAndPermissions\RoleResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -21,6 +22,7 @@ class UserResource extends JsonResource
             'email_verified_at' => $this->email_verified_at,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'role' => new RoleResource($this->whenLoaded('role'))
         ];
     }
 }
