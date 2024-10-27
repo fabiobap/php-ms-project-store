e7333b2590253d4efea204d70c98ec37b31b440e
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-26 23:34:20 -0300

    updating seeder

--- database/seeders/CategoryProductSeeder.php
+++ database/seeders/CategoryProductSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class CategoryProductSeeder extends Seeder
@@ -13,6 +15,13 @@ class CategoryProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory()->count(10)->create();
+        $cats = Category::factory()->count(10)->create();
+        Product::factory()
+            ->count(60)
+            ->state(new Sequence(
+                fn (Sequence $sequence) => ['category_id' => $cats->random()],
+            ))
+            ->create();
+
     }
 }
