592a27d4a499345d9649ea2d2448af3a61e7c1d4
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-26 23:33:46 -0300

    missing uuid on fillable products
    also observer for img

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use App\Models\Traits\HasExternalId;
 use App\Models\Traits\SlugHandler;
+use App\Observers\ProductObserver;
 use Database\Factories\ProductFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[ObservedBy([ProductObserver::class])]
 class Product extends Model
 {
     /** @use HasFactory<ProductFactory> */
@@ -20,6 +23,7 @@ class Product extends Model
         'price',
         'image',
         'category_id',
+        'uuid',
     ];
 
     public function category(): BelongsTo
