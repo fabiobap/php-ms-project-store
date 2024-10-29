e8ee3255f7439a945c8bd7cf6290e3ff75334eec
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-29 15:46:16 -0300

    migrations

--- database/migrations/2024_10_25_005813_create_products_table.php
+++ database/migrations/2024_10_25_005813_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description');
-            $table->integer('price');
+            $table->integer('amount');
             $table->string('image')->nullable();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
---
+++ database/migrations/2024_10_29_020543_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('external_id');
+            $table->string('status')->default(OrderStatus::PENDING->value);
+            $table->integer('amount')->default(0);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
