d7c14de5399e74f4222be9e76c77d59ba2d422aa
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-11-16 22:56:44 -0300

    removing data wrap from order

--- app/Http/Resources/Public/Order/OrderResource.php
+++ app/Http/Resources/Public/Order/OrderResource.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource
 {
+    public static $wrap = null;
+
     /**
      * Transform the resource into an array.
      *
